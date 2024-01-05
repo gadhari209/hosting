b9bff9ab5fd464e6992141d9c5887a03d6a36ee3
gadhari209 <gadhari209@gmail.com>
2024-01-05 23:20:43 +0530

    second commit

--- index.js
+++ index.js
@@ -1,15 +1,14 @@
+// backend/server.js
+const express = require('express');
+const cors = require('cors');
+const authRoutes = require('./routes/authRoutes');
 
-require('dotenv').config()
-const express = require('express')
-const app = express()
-
+const app = express();
 const PORT = process.env.PORT || 3001;
 
-app.get('/', (req, res) => {
-  res.send('hello world')
-})
+app.use(cors());
+app.use(express.json());
 
+app.use('/auth', authRoutes);
 
-app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
-  });
+module.exports = app;
