6b1b03f91a4dc4fb7f7af2aa4d6349f5a351a0f5
gadhari209 <gadhari209@gmail.com>
2024-01-06 10:35:05 +0530

    4th commit

--- database/db.js
+++ database/db.js
@@ -1,10 +1,10 @@
 const mysql = require('mysql2');
 
 const db = mysql.createConnection({
-  host: process.env.DB_HOST ,
-  user: process.env.DB_USER ,
-  password: process.env.DB_PASSWORD ,
-  database: process.env.DB_DATABASE ,
+  host: process.env.DB_HOST ||bpihkezkqzqabr8yvjeq-mysql.services.clever-cloud.com,
+  user: process.env.DB_USER ||ude5vjakckgkhsvv ,
+  password: process.env.DB_PASSWORD||WWAUoCIyIAmZEuHb75EU ,
+  database: process.env.DB_DATABASE ||bpihkezkqzqabr8yvjeq,
   port: process.env.DB_PORT || 3306,
 });
 
